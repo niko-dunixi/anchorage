dddea97468d3d017d1c4b4d97ff74a6fdccb8556
Paul FREAKN Baker <paul.nelson.baker@gmail.com>
2021-09-14 23:16:05 -0600

    SSH script to bootstrap arch

--- Makefile
+++ Makefile
@@ -24,10 +24,10 @@ out/user-data:
 out/cloud-init.iso: out/meta-data out/user-data
 	xorriso -as genisoimage -output $@ -volid CIDATA -joliet -rock $^
 
-out/main.img: out/bootstrapper out/linux.iso out/cloud-init.iso
+out/main.img: out/linux.iso out/cloud-init.iso
 	qemu-img create -f qcow2 $@ 15G
-	./out/bootstrapper || (rm -f "$@" && exit 1)
-	@# ./bootstrap.sh || (rm -f "$@" && exit 1)
+	# ./out/bootstrapper || (rm -f "$@" && exit 1)
+	./bootstrap.sh || (rm -f "$@" && exit 1)
 
 clean:
 	[ ! -d out ] || rm -rf out
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,29 +10,50 @@ set -euxo pipefail
 # trap cleanup EXIT
 
 qemu-system-x86_64 \
+  -display none \
   -m 8G \
   -drive file=./out/linux.iso,index=0,media=cdrom \
   -drive file=./out/cloud-init.iso,index=1,media=cdrom \
   -drive file=./out/main.img,if=virtio \
   -net user,hostfwd=tcp::10022-:22 \
   -net nic &
+sleep 10s
 
 function qemu-ssh {
-  ssh -q -o ConnectTimeout=5 -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" -p 10022 root@localhost ${@}
+  ssh -q -o ConnectTimeout=3 -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" -p 10022 root@localhost ${@}
 }
 
+set +x
+printf 'Waiting for SSH to go live (this will take a while)...'
 until qemu-ssh exit; do
-  sleep 1s
+  printf '.'
 done
 clear
 echo "CONNECTED!"
-# Format and install archlinux
-# qemu-ssh "printf 'gn\n\n\nw' | fdisk /dev/vda"
-qemu-ssh "mkfs.ext4 /dev/vda"
-qemu-ssh "mount /dev/vda /mnt"
-qemu-ssh "pacstrap /mnt base docker linux linux-firmware"
+USERNAME="docker"
+HOSTNAME="qemu"
+qemu-ssh << EOF
+mkfs.ext4 /dev/vda
+mount /dev/vda /mnt
+pacstrap /mnt base docker linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+echo "${HOSTNAME}" > /etc/hostname
+cat > /etc/hosts <<EOHOSTS
+127.0.0.1	localhost
+::1		localhost
+127.0.1.1	${HOSTNAME}.localdomain	${HOSTNAME}
+EOHOSTS
+cat > /etc/systemd/system/docker.service.d/execstart.conf <<EOCONFIG
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:4243
+EOCONFIG
+useradd docker-user
+usermod -aG docker docker-user
+systemctl enable docker.service
+exit
+shutdown
+EOF
 
-while qemu-ssh exit; do
-  qemu-ssh
-done
 wait
